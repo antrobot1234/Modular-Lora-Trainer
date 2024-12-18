9b6b2f49ce20dafc7ea1fe182cc705f0f6833251
antrobot1234 <antrobot1234@gmail.com>
2024-12-17 23:35:45 -0800

    allows utils to pull from outside of it's current dir to reach ryaml directory (kinda hacky)

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,12 @@ def write_yaml(path: str, dict: dict) -> None:
     with open(path, 'w') as f:
         yaml.dump(dict, f)
 def stack_yamls(paths: list[str],root_dir: str = None) -> dict:
+    current_dir = os.getcwd()
+    parent_dir = os.path.dirname(current_dir)
+    yaml_dir = os.path.join(parent_dir,root_dir) #this is kind of hacky and could get changed later
     out = {}
     if root_dir is not None:
-        paths = [os.path.join(root_dir,path) for path in paths]
+        paths = [os.path.join(yaml_dir,path) for path in paths]
     for path in paths:
         out.update(read_yaml(path))
     return out
